5a55b6659e536ddd69018736d8558a59b683ac55
devneri <dev@filipponeri.net>
2019-04-12 14:51:55 +0200

    Updated README.md

--- README.md
+++ README.md
@@ -2,7 +2,6 @@
 A class to scrape and extract data from AIFA (Agenzia Italiana del FArmaco) database using its public web service
 
 ## Use example
-
 ```
 <?php
 
--- src/Scraper.php
+++ src/Scraper.php
@@ -21,16 +21,30 @@ class Scraper {
     *
     * @param array $config Parsed INI file
     */
-    function __construct($config) {
+    function __construct($config = []) {
+        if (!isset($config['baseurl'])) {
+            $config['baseurl'] = 'https://www.agenziafarmaco.gov.it/services/search/select';
+        }
+
+        if (!isset($config['fieldlist'])) {
+            $config['fieldlist'] = '*';
+        }
+
+        if (!isset($config['maxrows'])) {
+            $config['maxrows'] = 2;
+        }
+
         $this->config = $config;
     }
 
-    private function buildAifaUrl($queryParams, $fieldList = null, $format = 'json', $maxRows = 100) {
+    private function buildAifaUrl($queryParams, $fieldList = null, $format = 'json') {
         $url = $this->config['baseurl'] . '?q=%s&fl=%s&wt=%s&rows=%d';
 
         if (\is_array($queryParams)) {
             $queryParams = str_replace('=', ':', \http_build_query($queryParams, null, '+'));
         }
+
+        $queryParams .= '+bundle:confezione_farmaco';
 
         if (\is_array($fieldList)) {
             $fieldList = implode(',', $fieldList);
@@ -39,7 +53,7 @@ class Scraper {
             $fieldList = $this->config['fieldlist'];
         }
 
-        return sprintf($url, $queryParams, $fieldList, $format, $maxRows);
+        return sprintf($url, $queryParams, $fieldList, $format, $this->config['maxrows']);
     }
 
     private function decodeHtmlEntities($rows) {
@@ -53,46 +67,61 @@ class Scraper {
         return $result;
     }
 
+    private function query($url) {
+        $result = json_decode(file_get_contents($url))->response->docs;
+        $result = $this->decodeHtmlEntities($result);
+
+        return $result;
+    }
+
     /**
     * Search by ATC
     *
     * Search AIFA database by ATC code and return array of records found
     *
-    * @param string $atc ATC code
+    * @param string $atc    ATC code (use of * wildcard is allowed)
+    * @param string $state  Administrative state of drug - 'A' Authorized / 'R' Revoked / 'S' Suspended / '*' all
     */
-    public function searchByAtc($atc, $state) {
+    public function searchByAtc($atc, $state = '*') {
         $url = $this->buildAifaUrl([
-            'sm_field_codice_atc' => $atc . '*',
+            'sm_field_codice_atc' => $atc,
             'sm_field_stato_farmaco' => $state
         ]);
 
-        $result = json_decode(file_get_contents($url))->response->docs;
-        $result = $this->decodeHtmlEntities($result);
-
-        return $result;
+        return $this->query($url);
     }
 
-    public function searchByName($name, $state) {
+    /**
+    * Search by drug name
+    *
+    * Search AIFA database by drug name and return array of records found
+    *
+    * @param string $name   Drug name (use of * wildcard is allowed)
+    * @param string $state  Administrative state of drug - 'A' Authorized / 'R' Revoked / 'S' Suspended / '*' all
+    */
+    public function searchByName($name, $state = '*') {
         $url = $this->buildAifaUrl([
-            'sm_field_descrizione_farmaco' => $name . '*',
+            'sm_field_descrizione_farmaco' => $name,
             'sm_field_stato_farmaco' => $state
         ]);
 
-        $result = json_decode(file_get_contents($url))->response->docs;
-        $result = $this->decodeHtmlEntities($result);
-
-        return $result;
+        return $this->query($url);
     }
 
-    public function searchByPrinciple($principle, $state) {
+    /**
+    * Search by drug active principle
+    *
+    * Search AIFA database by active principle and return array of records found
+    *
+    * @param string $principle  Active principle (use of * wildcard is allowed)
+    * @param string $state      Administrative state of drug - 'A' Authorized / 'R' Revoked / 'S' Suspended / '*' all
+    */
+    public function searchByPrinciple($principle, $state = '*') {
         $url = $this->buildAifaUrl([
-            'sm_field_descrizione_atc' => $principle . '*',
+            'sm_field_descrizione_atc' => $principle,
             'sm_field_stato_farmaco' => $state
         ]);
 
-        $result = json_decode(file_get_contents($url))->response->docs;
-        $result = $this->decodeHtmlEntities($result);
-
-        return $result;
+        return $this->query($url);
     }
 }
